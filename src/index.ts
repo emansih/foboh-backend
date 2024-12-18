184a134e13dbb3aab3cef22c9e598d8102530ebd
Daniel Quah <danielquah148@yahoo.com>
2024-12-18 17:09:45 +1100

    Enable CORS for all routes

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,10 @@ import express from "express";
 import productsRouter from "./routes/ProductRoutes";
 import pricingRouter from "./routes/PricingRoutes";
 
+var cors = require('cors')
 const app = express();
+
+app.use(cors())
 
 app.use(express.json());
 const port = 3000
